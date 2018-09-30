dd79dd60e7811793762f685b5133586761d67823
Andrew Rowley <andrew@blackhillsoftware.com>
2018-09-30 10:48:02 +1000

    Work on JobsByJobname

--- java/src/main/java/com/smfreports/JobsByJobname.java
+++ java/src/main/java/com/smfreports/JobsByJobname.java
@@ -1,37 +1,41 @@
 package com.smfreports;
 
 import java.io.IOException;
-import java.time.*;
 import java.util.*;
 
 import com.blackhillsoftware.smf.*;
-import com.blackhillsoftware.smf.smf30.IoActivitySection;
-import com.blackhillsoftware.smf.smf30.ProcessorAccountingSection;
 import com.blackhillsoftware.smf.smf30.Smf30Record;
 
 public class JobsByJobname
 {
     public static void main(String[] args) throws IOException
     {
-
+    	// A map of Job Names to JobData entries to collect information about that
+    	// group of jobs.
+    	
         Map<String, JobData> jobs = new HashMap<String, JobData>();
 
+        // If we received no arguments, open DD INPUT
+        // otherwise use the argument as a file name.
+        
         try (SmfRecordReader reader = 
                 args.length == 0 ?
                 SmfRecordReader.fromDD("INPUT") :
                 SmfRecordReader.fromName(args[0])) 
         {  
             reader
-                .include(30, 5)
+                .include(30, 5) // SMF 30 subtype 5 = Job End records
                 .stream()
                 .map(record -> new Smf30Record(record))
-                .filter(r30 -> r30.header().smf30con() != 0 || r30.header().smf30uon() != 0)
-                .filter(r30 -> r30.identificationSection().smf30cl8().equals("A"))
+                // Optionally filter here, e.g. to include only jobs running in 
+                // job class A:
+                //.filter(r30 -> r30.identificationSection().smf30cl8().equals("A"))
                 .forEach(r30 ->
                 {
-                    JobData job = jobs.computeIfAbsent(r30.identificationSection().smf30jbn(), x -> new JobData());
-                    job.add(Utils.oneOrNull(r30.processorAccountingSections()));    
-                    job.add(Utils.oneOrNull(r30.ioActivitySections()));          
+                    JobData job = 
+                    	jobs.computeIfAbsent(r30.identificationSection().smf30jbn(),
+                    	x -> new JobData());
+                    job.add(r30);           
                 });
         
         }
@@ -39,68 +43,92 @@ public class JobsByJobname
         writeReport(jobs);
     }
 
+    /**
+     * Write the report
+     * 
+     * @param jobs The map of Job Names to Job Data 
+     */
     private static void writeReport(Map<String, JobData> jobs)
     {
         // Headings
-        System.out.format("%n%-8s %5s %11s %11s %11s %11s %11s %11s %11s %11s%n", 
+        System.out.format("%n%-8s %6s %14s %14s %14s %14s %14s %14s %14s %14s%n", 
                 "Name", "Count", 
                 "CPU", "zIIP", "Connect", "Excp",
                 "Avg CPU", "Avg zIIP", "Avg Connect", "Avg Excp");
+               
         jobs.entrySet().stream()
-           .sorted((a, b) -> b.getValue().cpTime.compareTo(a.getValue().cpTime))
+        	// sort by CP Time 
+        	// reverse a and b in the comparison so sort is descending
+           .sorted((a, b) -> Float.compare(b.getValue().cpTime, a.getValue().cpTime))
+           .limit(100) // take top 100
            .forEachOrdered(jobname -> 
            {
                JobData jobinfo = jobname.getValue();    
                // write detail line     
                System.out.format(
-                       "%-8s %5d %11s %11s %11s %11d %11s %11s %11s %11d%n",
+                       "%-8s %,6d %14s %14s %14s %,14d %14s %14s %14s %,14d%n",
                        jobname.getKey(), 
                        jobinfo.count,
                        hhhmmss(jobinfo.cpTime),
                        hhhmmss(jobinfo.ziipTime),
                        hhhmmss(jobinfo.connectTime),
                        jobinfo.excps,
-                       hhhmmss(jobinfo.cpTime.dividedBy(jobinfo.count)),
-                       hhhmmss(jobinfo.ziipTime.dividedBy(jobinfo.count)),
-                       hhhmmss(jobinfo.connectTime.dividedBy(jobinfo.count)),
+                       hhhmmss(jobinfo.cpTime / jobinfo.count),
+                       hhhmmss(jobinfo.ziipTime / jobinfo.count),
+                       hhhmmss(jobinfo.connectTime / jobinfo.count),
                        jobinfo.excps/jobinfo.count);                     
            });
     }
 
-    private static String hhhmmss(Duration dur)
+    /**
+     * Format a duration as hhh:mm:ss. Fractional seconds
+     * are truncated to whole seconds.
+     * 
+     * @param duration
+     * @return The formatted duration.
+     */
+    private static String hhhmmss(float duration)
     {
-        long hours = dur.toHours();
-        long minutes = dur.minus(Duration.ofHours(hours)).toMinutes();
-        long seconds = dur.minus(Duration.ofHours(hours))
-                .minus(Duration.ofMinutes(minutes)).toMillis() / 1000;
-        return String.format("%d:%02d:%02d", hours, minutes, seconds);
+    	final int SECONDS_PER_MINUTE = 60;
+    	final int SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
+    	
+        int hours = (int)(duration / SECONDS_PER_HOUR);
+        int minutes = (int)((duration % SECONDS_PER_HOUR)) / SECONDS_PER_MINUTE;
+        float seconds = duration % SECONDS_PER_MINUTE;
+        
+        return String.format("%d:%02d:%05.2f", hours, minutes, seconds);
     }
 
+    /**
+     * A class to accumulate information about
+     * a group of jobs.
+     */
     private static class JobData
     {
-        public void add(ProcessorAccountingSection proc)
+    	/**
+    	 * Add information from a SMF 30 record.
+    	 * @param r30 The Smf30Record
+    	 */
+        public void add(Smf30Record r30)
         {
-            if (proc != null)
+            if (r30.processorAccountingSection() != null)
             {
-                count++;
-                cpTime = cpTime.plus(proc.smf30cpt()).plus(proc.smf30cps());
-                ziipTime = ziipTime.plus(proc.smf30TimeOnZiip());
+                count++; // pick a section that only occurs once and use to count jobs
+                cpTime += r30.processorAccountingSection().smf30cptSeconds()
+                		+ r30.processorAccountingSection().smf30cpsSeconds();
+                ziipTime += r30.processorAccountingSection().smf30TimeOnZiipSeconds();
+            }
+            if (r30.ioActivitySection() != null)
+            {
+                excps += r30.ioActivitySection().smf30tex();
+                connectTime += r30.ioActivitySection().smf30aicSeconds();
             }
         }
-        
-        public void add(IoActivitySection ioact)
-        {
-            if (ioact != null)
-            {
-                excps += ioact.smf30tex();
-                connectTime = connectTime.plus(ioact.smf30aic());
-            }
-        }
-
+       
         int count = 0;
-        Duration cpTime = Duration.ZERO;
-        Duration ziipTime = Duration.ZERO;       
-        Duration connectTime = Duration.ZERO;
+        float cpTime = 0;
+        float ziipTime = 0;       
+        float connectTime = 0;
         long excps = 0;
     }
 
