d6493b6b719fc6dd86a3edd1e473313192753166
Andrew Rowley <andrew@blackhillsoftware.com>
2019-09-05 15:31:10 +1000

    Correct comments

--- tutorial/scripts/runsample.bat
+++ tutorial/scripts/runsample.bat
@@ -5,11 +5,11 @@
 @REM        or permanent key.
 @REM        Get a 30 day trial key from: 
 @REM        https://www.blackhillsoftware.com/30-day-trial/
-@REM      3) If you compiled your samples using the compilesample.bat script, 
-@REM         change TARGET to match compilesample.bat. If the specified class 
-@REM         is not found in the TARGET directory, the EasySMFSamples jar
-@REM         will be searched.
-@REM     3) Run the batch file passing the sample and SMF data file as arguments e.g.:
+@REM     3) If you compiled your samples using the compilesample.bat script, 
+@REM        change TARGET to match compilesample.bat. If the specified class 
+@REM        is not found in the TARGET directory, the EasySMFSamples jar
+@REM        will be searched.
+@REM     4) Run the batch file passing the sample and SMF data file as arguments e.g.:
 @REM        runsample com.blackhillsoftware.samples.RecordCount SMF.DATA
 @REM
 @REM     This sample assumes that required Java environment variables e.g. JAVA_HOME 
