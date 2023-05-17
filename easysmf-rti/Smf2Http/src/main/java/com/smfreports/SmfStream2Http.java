d0344df0150368c0dbf531ef6ce7fb8dfcd65186
Andrew Rowley <andrew@blackhillsoftware.com>
2023-05-17 16:59:06 +1000

    Change to use Java 11 HttpClient

--- easysmf-rti/Smf2Http/src/main/java/com/smfreports/SmfStream2Http.java
+++ easysmf-rti/Smf2Http/src/main/java/com/smfreports/SmfStream2Http.java
@@ -2,15 +2,20 @@ package com.smfreports;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.impl.classic.*;
-import org.apache.hc.core5.http.*;
-import org.apache.hc.core5.http.io.entity.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpRequest.Builder;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 import com.blackhillsoftware.smf.realtime.*;
 
 public class SmfStream2Http
 {
-	public static void main(String[] args) throws IOException, InterruptedException 
+	public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException 
 	{		    
 		try (SmfConnection rti = 
 				SmfConnection
@@ -20,37 +25,52 @@ public class SmfStream2Http
 					.connect())
 		{	
 		    ByteArrayOutputStream queuedRecords = new ByteArrayOutputStream();
-			
-	    	try (CloseableHttpClient httpclient = HttpClients.createDefault()) 
-	    	{
-	    	    HttpPost httpPost = new HttpPost(args[1]);
-			
-				for (byte[] record : rti)
+		    
+	        HttpClient client = HttpClient.newBuilder().build();    
+		    Builder requestBuilder = HttpRequest.newBuilder(new URI(args[1]));
+					
+			for (byte[] record : rti)
+			{
+				queuedRecords.write(record);
+				if (!rti.hasNextImmediately() 
+						|| queuedRecords.size() > 10 * 1024 * 1024) // nothing more currently available, or 10MB queued  
 				{
-					queuedRecords.write(record);
-					if (!rti.hasNextImmediately() 
-							|| queuedRecords.size() > 10 * 1024 * 1024) // nothing more currently available, or 10MB queued  
+					byte[] payload = queuedRecords.toByteArray();
+					queuedRecords= new ByteArrayOutputStream();
+					
+					// http post records
+					
+					HttpRequest request = requestBuilder.POST(BodyPublishers.ofByteArray(payload))
+					    .header("Content-Type", "application/octet-stream")
+					    .build();
+					
+					try
 					{
-						byte[] payload = queuedRecords.toByteArray();
-						queuedRecords= new ByteArrayOutputStream();
-						
-						// http post records
-		            	httpPost.setEntity(new ByteArrayEntity(payload, ContentType.APPLICATION_OCTET_STREAM));
-		                try (CloseableHttpResponse response = httpclient.execute(httpPost)) 
-		                {
-		                	if (response.getCode() != 200)
-		                	{
-		                		System.out.println(response.getCode() + " " + response.getReasonPhrase());
-		                	}
-		                    HttpEntity entity2 = response.getEntity();
-		                    EntityUtils.consume(entity2);
-		                }
-					}
+					    send(client, request);
+                    }
+                    catch (IOException e)
+                    {
+                        System.out.println(e.toString());
+                        System.out.println("will retry in 5 seconds");
+                        Thread.sleep(5000);
+                        // send the same request again
+                        // if this fails too we allow the exception to propagate
+                        send(client, request);
+                    }	                
 				}
-	    	}
-		// Connection closed & resources freed automatically at the end of the try block 	
+			}	    	
 		} 	
-	}	
+	}
+
+    private static void send(HttpClient client, HttpRequest request) throws IOException, InterruptedException {
+        HttpResponse<String> response =
+            client.send(request, BodyHandlers.ofString());
+
+        if (response.statusCode() != 200)
+        {
+        	System.out.println(response.statusCode() + " " + response.body());
+        }
+    }	
 	
 	static void handleMissedData(MissedDataEvent e)
 	{
